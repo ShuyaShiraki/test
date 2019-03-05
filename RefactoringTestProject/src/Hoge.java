afe1d9485a207cc07cc6a61548440d300a7cacfc
s-sirakiLab <s-siraki@ist.osaka-u.ac.jp>
2019-03-05 16:30:06 +0900

    create Java project

---
+++ RefactoringTestProject/src/Hoge.java
@@ -0,0 +1,13 @@
+
+public class Hoge {
+
+	public static void main(String[] args) {
+		Hoge hoge = new Hoge();
+		hoge.HelloWorld();
+	}
+
+	public void HelloWorld() {
+		System.out.println("Hello World");
+	}
+
+}
